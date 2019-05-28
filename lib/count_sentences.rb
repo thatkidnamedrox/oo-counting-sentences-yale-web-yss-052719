d6999344389d5cd79c1c705ea9b1d24904a3ebc8
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 18:08:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[\.?!]/).delete_if {|c| c == ""}
+    split_sentence.count
   end
 end
